105d5adf5034e3a542ac4efa3e2712e98f3bf40b
1506436 <I.Ahmed13@wlv.ac.uk>
2019-03-07 11:57:22 +0000

    Added nav bar

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -13,5 +13,27 @@ $matches = json_decode($response); ?>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
         </head>
         <body>
-
-                <h1><?php echo $title; ?></h1>
+<nav class="navbar navbar-expand-lg navbar-light bg-light static-top">
+<div class="container">
+<a class="navbar-brand" href="#">Football Centre</a>
+<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+<span class="navbar-toggler-icon"></span>
+</button>
+<div class="collapse navbar-collapse" id="navbarResponsive">
+<ul class="navbar-nav ml-auto">
+<li class="nav-item">
+<a class="nav-link" href="#">Home</a>
+</li>
+<li class="nav-item">
+<a class="nav-link" href="#">Standings</a>
+</li>
+<li class="nav-item">
+<a class="nav-link" href="#">Results</a>
+</li>
+<li class="nav-item">
+<a class="nav-link" href="#">Contact</a>
+</li>
+</ul>
+</div>
+</div>
+</nav>
