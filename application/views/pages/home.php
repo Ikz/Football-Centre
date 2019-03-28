a0f378a8a5675c147f708949525523e6a4fb0c0d
1506436 <I.Ahmed13@wlv.ac.uk>
2019-03-28 12:22:23 +0000

    Changed the home page and added an image in

--- application/views/pages/home.php
+++ application/views/pages/home.php
@@ -1,4 +1,43 @@
-<?php
-echo "Hello World!";
-    
-?>
+<!-- Page Content -->
+<div class="container">
+
+<!-- Portfolio Item Heading -->
+<h1 class="my-4">Page Heading
+<small>Secondary Text</small>
+</h1>
+
+<!-- Portfolio Item Row -->
+<div class="row">
+
+<div class="col-md-8">
+<img class="img-fluid" src="<?php echo base_url(); ?>img/stadium.jpg" alt="">
+</div>
+
+<div class="col-md-4">
+<h3 class="my-3">Project Description</h3>
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam viverra euismod odio, gravida pellentesque urna varius vitae. Sed dui lorem, adipiscing in adipiscing et, interdum nec metus. Mauris ultricies, justo eu convallis placerat, felis enim.</p>
+<h3 class="my-3">Project Details</h3>
+<ul>
+<li>Lorem Ipsum</li>
+<li>Dolor Sit Amet</li>
+<li>Consectetur</li>
+<li>Adipiscing Elit</li>
+</ul>
+</div>
+
+</div>
+<!-- /.row -->
+
+<!-- Related Projects Row -->
+<h3 class="my-4">Premier League Table</h3>
+
+
+
+<div class="resp-container">
+<iframe class="resp-iframe" frameborder="0"  scrolling="auto" width="100%" height="100%" style="height:700px; width:100%;" src="https://www.fctables.com/england/premier-league/iframe/?type=table&lang_id=2&country=67&template=10&team=&timezone=Europe/London&time=24&po=1&ma=1&wi=1&dr=1&los=1&gf=1&ga=1&gd=1&pts=1&ng=1&form=1&width=1000&height=700&font=Arial&fs=12&lh=24&bg=FFFFFF&fc=000000&logo=1&tlink=0&ths=1&thb=1&thba=FFFFFF&thc=000000&bc=ffffff&hob=ccd0d4&hobc=00ffff&lc=7bff00&sh=1&hfb=1&hbc=00ffff&hfc=000"></iframe>
+</div>
+
+<!-- /.row -->
+
+</div>
+<!-- /.container -->
