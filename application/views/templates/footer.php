bff06f1a45719f3350456b1d243a42ff14408bd4
1506436 <I.Ahmed13@wlv.ac.uk>
2019-03-28 12:23:01 +0000

    Added new footer

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -1,3 +1,7 @@
-                <br><em>&copy; 2019 Ikhlas Ahmed</em>
+<footer id="sticky-footer" class="py-4 bg-light text-black-50">
+<div class="container text-center">
+<small>Copyright &copy; 2019 Ikhlas Ahmed - 1506436</small>
+</div>
+</footer>
         </body>
 </html>
