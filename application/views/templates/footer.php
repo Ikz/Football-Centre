a5b2d9f9e666bab04d480d204f9236d5a12c6aa9
1506436 <I.Ahmed13@wlv.ac.uk>
2019-04-04 10:26:54 +0100

    Modified footer to enable collapsable nav bar on mobile
    Added JavaScript and JQuery to enable extra features including collapsable navigation bar on mobile view

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -3,5 +3,10 @@
 <small>Copyright &copy; 2019 Ikhlas Ahmed - 1506436</small>
 </div>
 </footer>
+<!-- Bootstrap core JavaScript-->
+<script src="<?php echo base_url(); ?>vendor/jquery/jquery.min.js"></script>
+<script src="<?php echo base_url(); ?>vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+<!-- Core plugin JavaScript-->
+<script src="<?php echo base_url(); ?>vendor/jquery-easing/jquery.easing.min.js"></script>
         </body>
 </html>
