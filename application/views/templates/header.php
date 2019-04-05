ad70da14148ee5cfdb526e734071198b080c4f21
1506436 <I.Ahmed13@wlv.ac.uk>
2019-04-05 18:44:35 +0100

    Edited links and added .htaccess

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -5,6 +5,8 @@
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 <link href="<?php echo base_url(); ?>css/style.css" type="text/css" rel="stylesheet">
+<link href="<?php echo base_url(); ?>vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.bundle.min.js" integrity="sha384-xrRywqdh3PHs8keKZN+8zzc5TX0GRTLCcmivcbNJWm2rs5C8PRhcEn3czEjhAO9o" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
@@ -12,23 +14,20 @@
         <body>
 <nav class="navbar navbar-expand-lg navbar-light bg-light static-top">
 <div class="container">
-<a class="navbar-brand" href="#">Football Centre</a>
+<a class="navbar-brand" href="home">Football Centre</a>
 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
 <span class="navbar-toggler-icon"></span>
 </button>
 <div class="collapse navbar-collapse" id="navbarResponsive">
 <ul class="navbar-nav ml-auto">
 <li class="nav-item">
-<a class="nav-link" href="#">Home</a>
+<a class="nav-link" href="home">Home</a>
 </li>
 <li class="nav-item">
-<a class="nav-link" href="#">Standings</a>
+<a class="nav-link" href="standings">Standings</a>
 </li>
 <li class="nav-item">
-<a class="nav-link" href="#">Results</a>
-</li>
-<li class="nav-item">
-<a class="nav-link" href="#">Contact</a>
+<a class="nav-link" href="login">Login</a>
 </li>
 </ul>
 </div>
